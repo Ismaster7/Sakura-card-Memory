dc5105f8397c111cbe97dc18f7e250100a4473ab
Ismael C Lima <ismael.lima2000@outlook.com.br>
2024-01-22 12:09:54 -0300

    Projeto finalizado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,83 @@
 
-
 const cards = [
-    1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9
+    {
+        numero: 1,
+        imagem: "background-image: url('./assets/image/cartas/1.jpg')",
+    },
+    {
+        numero: 1,
+        imagem: "background-image: url('./assets/image/cartas/1.jpg')",
+    },
+    {
+        numero: 2,
+        imagem: "background-image: url('./assets/image/cartas/2.jpg')",
+    },
+    {
+        numero: 2,
+        imagem: "background-image: url('./assets/image/cartas/2.jpg')",
+    },
+    {
+        numero: 3,
+        imagem: "background-image: url('./assets/image/cartas/3.jpg')",
+    },
+    {
+        numero: 3,
+        imagem: "background-image: url('./assets/image/cartas/3.jpg')",
+    },
+    {
+        numero: 4,
+        imagem: "background-image: url('./assets/image/cartas/4.jpg')",
+    },
+    {
+        numero: 4,
+        imagem: "background-image: url('./assets/image/cartas/4.jpg')",
+    },
+    {
+        numero: 5,
+        imagem: "background-image: url('./assets/image/cartas/5.jpg')",
+    },
+    {
+        numero: 5,
+        imagem: "background-image: url('./assets/image/cartas/5.jpg')",
+    },
+    {
+        numero: 6,
+        imagem: "background-image: url('./assets/image/cartas/6.jpg')",
+    },
+    {
+        numero: 7,
+        imagem: "background-image: url('./assets/image/cartas/7.jpg')",
+    },
+    {
+        numero: 7,
+        imagem: "background-image: url('./assets/image/cartas/7.jpg')",
+    },
+    {
+        numero: 8,
+        imagem: "background-image: url('./assets/image/cartas/8.jpg')",
+    },
+    {
+        numero: 8,
+        imagem: "background-image: url('./assets/image/cartas/8.jpg')",
+    },
+    {
+        numero: 9,
+        imagem: "background-image: url('./assets/image/cartas/9.jpg')",
+    },
+    {
+        numero: 9,
+        imagem: "background-image: url('./assets/image/cartas/9.jpg')",
+    },
+    {
+        numero: 6,
+        imagem: "background-image: url('./assets/image/cartas/6.jpg')",
+    },
+
 ]
 
 let openCards = [
-
-
 ]
+
 
 let shuffleCards = cards.sort(() =>{
    return Math.random() > 0.5 ? 2 : -1;
@@ -17,19 +87,42 @@ function sortear(){
     for(let i = 0; i < cards.length; i++){
         let box = document.createElement("div");
         box.className = "item";
-        box.innerHTML = shuffleCards[i];
-      // let imagem = document.createElement("img")
-       //imagem.src = `url(.image/cartas/1.jpg)`
-       //${shuffleCards[i]}
-       //imagem.style.backgroundSize = "cover";
-     box.style.backgroundImage = 'url(./image/cartas/' + 1 + '.jpg)';
-        document.querySelector(".game").appendChild(box)
-     //   box.appendChild(imagem)
+        box.innerHTML = shuffleCards; //[i]
+        box.style = cards[i].imagem;
+        box.onclick = handleClick;
+        document.querySelector("#game").appendChild(box)
+        box.textContent = cards[i].numero;
+
     }
     
 }
 
+function handleClick(){
+    if(openCards.length < 2){
+        this.classList.add("boxOpen");
+        openCards.push(this)
+    }
+    if(openCards.length == 2){
+        setTimeout(checkMatch, 500);
+    }
+};
 
+function checkMatch(){
+    if(openCards[0].innerHTML === openCards[1].innerHTML){
+        console.log("vitoria")
+        openCards[0].classList.add("boxMatch");
+        openCards[1].classList.add("boxMatch");
+    }else{
+        openCards[0].classList.remove("boxOpen");
+        openCards[1].classList.remove("boxOpen");
+    }
+
+    openCards = [];
+    if(document.querySelectorAll(".boxMatch").length === cards.length){
+        alert("Parabens ! Você venceu.");
+        window.location.reload();
+    }
+}
 
 let carta = new Image()
 function trazerCarta(numeroCarta){
@@ -37,7 +130,7 @@ function trazerCarta(numeroCarta){
 }
 
 function main(){
-sortear(    )
+sortear()
 }
 
 main()
